9a8c2bbe2f4b7a16e616941e43b00c8d3fdfdcbe
LorneCyr <lorne.s.cyr@gmail.com>
2020-08-03 16:46:43 -0300

    Database setup completed.

---
+++ TodoListApp/TodoListApp/Migrations/20200803194254_AddTodoItemToDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoListApp.Migrations
+{
+    public partial class AddTodoItemToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+        }
+    }
+}
